b2a0c48a0e6b737b5a8dde946cccc7cc2c1f279d
16 Caracteres <camille@16caracteres.com>
2024-02-23 11:21:35 +0100

    Polices Google Fonts en local

--- wp-content/themes/chicdressing/functions.php
+++ wp-content/themes/chicdressing/functions.php
@@ -7,7 +7,13 @@ function chicdressing_enqueue_styles() {
 
 add_filter( 'big_image_size_threshold', '__return_false' );
 
-
+function remove_google_fonts() {  
+    wp_dequeue_style( 'ashe-playfair-font' );
+    wp_dequeue_style( 'ashe-opensans-font' );
+    wp_dequeue_style( 'ashe-kalam-font' );
+    wp_deregister_script('ashe_gfonts_scripts');
+}
+add_action( 'wp_enqueue_scripts','remove_google_fonts', 999 );
 
 
  
